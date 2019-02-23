774e085413ea874d89244ec402770a960c91317e
Johny Klein <johnyklein@hotmail.com>
2019-02-23 11:48:01 -0300

    Iniciando com Collections sort

---
+++ collections-java-util/src/br/com/johny/app/TesteListas.java
@@ -0,0 +1,30 @@
+package br.com.johny.app;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TesteListas {
+	public static ArrayList<String> materias = new ArrayList<String>();
+
+	public static void main(String[] args) {
+		getMaterias();
+		
+//		Collections.sort(materias, Collections.reverseOrder());
+		materias.sort(Collections.reverseOrder());
+		
+		materias.forEach(materias -> {
+			System.out.println("Materia = " + materias);
+		});
+		
+	}
+
+	private static void getMaterias() {
+		materias.add("Matematica");
+		materias.add("Portugues");
+		materias.add("Fisica");
+		materias.add("Quimica");
+		materias.add("Biologia");
+		materias.add("Artes");
+		materias.add("Historia");
+	}
+}
